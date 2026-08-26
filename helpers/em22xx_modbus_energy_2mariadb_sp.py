b3fdab4a86284179bdc7aace6ffbcb96260bef7a
Hasenradball <mail@frankhaefele.de>
2026-08-26 08:08:28 +0200

    add helper functions

---
+++ helpers/em22xx_modbus_energy_2mariadb_sp.py
@@ -0,0 +1,20 @@
+"""Script to read energy data from EM22xx energy meter and send to mariadb"""
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import mysql.connector
+from mysql.connector import errorcode
+from energymid_em22xx import EnergyMIDEM22xx
+from mariadb_config import MARIA_DB_CONFIG
+from maria_db_mysql import MariaDBMysql as maria_db
+
+# Main
+if __name__ == "__main__":
+    em2289_obj = EnergyMIDEM22xx("192.168.178.253")
+    energy_import = em2289_obj.get_energy_import_total()
+    energy_export = em2289_obj.get_energy_export_total()
+    energy = (energy_import, energy_export)
+    #print(energy)
+    maria_obj = maria_db(MARIA_DB_CONFIG)
+    maria_obj.insert_by_stored_procedure('insert_into_energie', energy)
+    #print("INFO: Insert into mariaDB finished!")
---
+++ helpers/test.py
@@ -0,0 +1,26 @@
+"""Script to test the ES2289 functionality"""
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import time
+from energymid_em22xx import EnergyMIDEM22xx
+
+# Main
+if __name__ == "__main__":
+    start = time.perf_counter()
+    em2289_obj = EnergyMIDEM22xx("192.168.178.253")
+    # just for testing
+    #em2289_obj.set_enable_webserver()
+    print(f'Webserver status: {em2289_obj.read_webserver_status()}')
+    print('\n')
+    #print(f'Voltages Primary: {em2289_obj.get_voltages_primary()} V')
+    #print(f'Currents Primary: {em2289_obj.get_currents_primary()} A')
+    #print(f'Power Primary   : {em2289_obj.get_power_primary()} W')
+    #print('\n')
+    #print(f'Energy import total : {em2289_obj.get_energy_import_total()} kWh')
+    #print(f'Energy export total : {em2289_obj.get_energy_export_total()} kWh')
+    em2289_obj.read_device_features()
+    #em2289_obj.read_device_information()
+    #em2289_obj.read_firmware_version()
+    stop = time.perf_counter()
+
+    print(f'\nINFO: test finished in {stop-start:.3f} s')
